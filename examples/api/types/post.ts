cb038e844d215319b01de0051bcaf10157eb7a51
seanng <shonum@gmail.com>
2021-08-21 10:07:44 +0800

    r: add existing backend to examples folder

---
+++ examples/api/types/post.ts
@@ -0,0 +1,23 @@
+import { objectType } from 'nexus'
+import prisma from 'lib/prisma'
+
+const Post = objectType({
+  name: 'Post',
+  definition(t) {
+    t.int('id')
+    t.string('title')
+    t.nullable.string('content')
+    t.boolean('published')
+    t.nullable.field('author', {
+      type: 'User',
+      resolve: parent =>
+        prisma.post
+          .findUnique({
+            where: { id: Number(parent.id) },
+          })
+          .author(),
+    })
+  },
+})
+
+export default Post
