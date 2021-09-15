feec6542f2e15c56d479090a38d7bb6cf8a9e4e8
seanng <shonum@gmail.com>
2021-09-15 21:27:48 +0800

    f: remove links from index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { GetServerSideProps, NextPage } from 'next'
-import Link from 'next/link'
 import Layout from 'components/organisms/Layout'
-import { Box, Container, Text } from '@chakra-ui/react'
+import { Container, Text } from '@chakra-ui/react'
 import { getUserPayload } from 'utils/auth'
 import { redirTo } from 'utils/helpers'
 
@@ -14,12 +13,6 @@ const IndexPage: NextPage = () => {
           if user is authenticated, (s)he will be automatically redirected to
           /dashboard (using getServerSideProps).
         </Text>
-        <Box my={2}>
-          <Link href="/dashboard-creator"> -&gt; Dashboard (creator)</Link>
-        </Box>
-        <Box my={2}>
-          <Link href="/dashboard-brand"> -&gt; Dashboard (brand)</Link>
-        </Box>
       </Container>
     </Layout>
   )
