aadd4e7ca10737f167b0d3fd68d076b79ace16cb
seanng <shonum@gmail.com>
2021-09-25 11:21:27 +0800

    f: open reel video in new tab

--- components/organisms/ProfileReel.tsx
+++ components/organisms/ProfileReel.tsx
@@ -1,5 +1,4 @@
 import { format } from 'date-fns'
-import NextLink from 'next/link'
 import {
   Grid,
   Button,
@@ -42,95 +41,91 @@ const ProfileReel = ({ profile }: Props): JSX.Element => {
               borderRadius="xl"
             >
               <LinkBox position="relative">
-                <NextLink href={item.url || '#'} passHref>
-                  <LinkOverlay>
-                    <Flex
-                      direction={[
-                        'row-reverse',
-                        'column-reverse',
-                        null,
-                        'row-reverse',
-                      ]}
-                      justify="space-between"
-                      position="absolute"
-                      p={3}
-                      w="full"
-                      h="full"
-                      alignItems="flex-start"
-                    >
-                      {item.companyName && (
-                        <Flex
-                          direction={['row', 'column', null, 'row']}
-                          alignItems="flex-start"
-                        >
-                          <Tag variant="reel" mr={1} mb={1}>
-                            {item.companyName}
-                          </Tag>
-                          <Tag variant="reel" mb={1}>
-                            {item.deliverable}
-                          </Tag>
-                        </Flex>
-                      )}
-                      {item.isVerified && (
-                        <Tag variant="reel" bgColor={green[600]}>
-                          <IsVerifiedTag color="black" />
+                <LinkOverlay href={item.url} isExternal>
+                  <Flex
+                    direction={[
+                      'row-reverse',
+                      'column-reverse',
+                      null,
+                      'row-reverse',
+                    ]}
+                    justify="space-between"
+                    position="absolute"
+                    p={3}
+                    w="full"
+                    h="full"
+                    alignItems="flex-start"
+                  >
+                    {item.companyName && (
+                      <Flex
+                        direction={['row', 'column', null, 'row']}
+                        alignItems="flex-start"
+                      >
+                        <Tag variant="reel" mr={1} mb={1}>
+                          {item.companyName}
                         </Tag>
-                      )}
-                    </Flex>
-                    <Center
-                      position="absolute"
-                      opacity={0}
-                      _hover={{ bgColor: 'rgba(0,0,0,0.7)', opacity: 1 }}
-                      w="full"
-                      h="full"
-                    >
-                      <Flex alignItems="center" direction="column">
-                        <Center
-                          borderRadius="full"
-                          bgColor="white"
-                          height="30px"
-                          width="30px"
-                          mb={3}
-                        >
-                          <Image
-                            src={`/logos/${item.platform}.png`}
-                            width="20px"
-                            height={
-                              item.platform === 'youtube' ? '15px' : '20px'
-                            }
-                          />
-                        </Center>
-                        {item.companyName && (
-                          <Text
-                            color="white"
-                            fontSize="20px"
-                            fontWeight="bold"
-                          >{`${item.deliverable} for ${item.companyName}`}</Text>
-                        )}
-                        <Text
-                          textStyle="base"
-                          mb={5}
-                          fontWeight="medium"
-                          color="white"
-                        >
-                          {format(new Date(item.publishedAt), 'dd LLLL yyyy')}
-                        </Text>
-                        <Button
-                          size="sm"
-                          bgColor="white"
-                          color="gray.900"
-                          type="button"
-                          textTransform="uppercase"
-                          fontWeight="bold"
-                          // mb={5} TODO: uncomment
-                        >
-                          Play Video
-                        </Button>
-                        {/* TODO: add video stats. */}
+                        <Tag variant="reel" mb={1}>
+                          {item.deliverable}
+                        </Tag>
                       </Flex>
-                    </Center>
-                  </LinkOverlay>
-                </NextLink>
+                    )}
+                    {item.isVerified && (
+                      <Tag variant="reel" bgColor={green[600]}>
+                        <IsVerifiedTag color="black" />
+                      </Tag>
+                    )}
+                  </Flex>
+                  <Center
+                    position="absolute"
+                    opacity={0}
+                    _hover={{ bgColor: 'rgba(0,0,0,0.7)', opacity: 1 }}
+                    w="full"
+                    h="full"
+                  >
+                    <Flex alignItems="center" direction="column">
+                      <Center
+                        borderRadius="full"
+                        bgColor="white"
+                        height="30px"
+                        width="30px"
+                        mb={3}
+                      >
+                        <Image
+                          src={`/logos/${item.platform}.png`}
+                          width="20px"
+                          height={item.platform === 'youtube' ? '15px' : '20px'}
+                        />
+                      </Center>
+                      {item.companyName && (
+                        <Text
+                          color="white"
+                          fontSize="20px"
+                          fontWeight="bold"
+                        >{`${item.deliverable} for ${item.companyName}`}</Text>
+                      )}
+                      <Text
+                        textStyle="base"
+                        mb={5}
+                        fontWeight="medium"
+                        color="white"
+                      >
+                        {format(new Date(item.publishedAt), 'dd LLLL yyyy')}
+                      </Text>
+                      <Button
+                        size="sm"
+                        bgColor="white"
+                        color="gray.900"
+                        type="button"
+                        textTransform="uppercase"
+                        fontWeight="bold"
+                        // mb={5} TODO: uncomment
+                      >
+                        Play Video
+                      </Button>
+                      {/* TODO: add video stats. */}
+                    </Flex>
+                  </Center>
+                </LinkOverlay>
                 <Image
                   src={item.thumbnailUrl}
                   alt="thumbnail"
