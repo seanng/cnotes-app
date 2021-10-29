fb1cd4938f8dff7f4db918754dddd0a753a603f0
seanng <shonum@gmail.com>
2021-10-29 16:37:42 +0800

    c: revert to named exports

--- backend/graphql/index.ts
+++ backend/graphql/index.ts
@@ -1,17 +1,6 @@
-import * as User from './User'
-import * as Auth from './Auth'
-import * as Listing from './Listing'
-import * as Offer from './Offer'
-import * as Custom from './Custom'
-import * as Deal from './Deal'
-
-const types = {
-  User,
-  Auth,
-  Listing,
-  Offer,
-  Custom,
-  Deal,
-}
-
-export default types
+export * from './User'
+export * from './Auth'
+export * from './Listing'
+export * from './Offer'
+export * from './Custom'
+export * from './Deal'
--- backend/schema.ts
+++ backend/schema.ts
@@ -1,12 +1,12 @@
 import { makeSchema } from 'nexus'
 import path from 'path'
-// import * as types from './graphql'
-import * as User from './graphql/User'
-import * as Auth from './graphql/Auth'
-import * as Listing from './graphql/Listing'
-import * as Offer from './graphql/Offer'
-import * as Custom from './graphql/Custom'
-import * as Deal from './graphql/Deal'
+import * as types from './graphql'
+// import * as User from './graphql/User'
+// import * as Auth from './graphql/Auth'
+// import * as Listing from './graphql/Listing'
+// import * as Offer from './graphql/Offer'
+// import * as Custom from './graphql/Custom'
+// import * as Deal from './graphql/Deal'
 // export * from './Auth'
 // export * from './Listing'
 // export * from './Offer'
@@ -15,14 +15,14 @@ import * as Deal from './graphql/Deal'
 
 // outputs a schemalink schema
 
-const types = {
-  User,
-  Auth,
-  Listing,
-  Offer,
-  Custom,
-  Deal,
-}
+// const types = {
+//   User,
+//   Auth,
+//   Listing,
+//   Offer,
+//   Custom,
+//   Deal,
+// }
 
 export const schema = makeSchema({ types })
 
