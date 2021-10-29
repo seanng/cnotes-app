626f375079da0a5400c1a04e3138b0e5397fb30c
seanng <shonum@gmail.com>
2021-10-29 16:09:21 +0800

    r: schema

--- api/schema.ts
+++ api/schema.ts
@@ -1,8 +1,29 @@
 import { makeSchema } from 'nexus'
 import path from 'path'
-import * as types from './graphql'
+// import * as types from './graphql'
+import * as User from './graphql/User'
+import * as Auth from './graphql/Auth'
+import * as Listing from './graphql/Listing'
+import * as Offer from './graphql/Offer'
+import * as Custom from './graphql/Custom'
+import * as Deal from './graphql/Deal'
+// export * from './Auth'
+// export * from './Listing'
+// export * from './Offer'
+// export * from './Deal'
+// export * from './Custom'
 
 // outputs a schemalink schema
+
+const types = {
+  User,
+  Auth,
+  Listing,
+  Offer,
+  Custom,
+  Deal,
+}
+
 export const schema = makeSchema({ types })
 
 // does not output a schemalink schema
