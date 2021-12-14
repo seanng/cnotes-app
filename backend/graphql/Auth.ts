632ddda1af7cde97b3c56aea67cdd266970c47c2
sean <shonum@gmail.com>
2021-12-14 14:29:35 +0800

    make websiteUrl input nullable

--- backend/graphql/Auth.ts
+++ backend/graphql/Auth.ts
@@ -176,11 +176,11 @@ export const SignupInput = inputObjectType({
   definition(t) {
     t.nonNull.string('email')
     t.nonNull.string('password')
-    t.nonNull.string('websiteUrl')
     t.nonNull.string('role')
     t.nonNull.string('firstName')
     t.nonNull.string('lastName')
     t.nonNull.string('alias')
+    t.string('websiteUrl')
   },
 })
 
